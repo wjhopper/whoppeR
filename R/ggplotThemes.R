c659e783f2de59f687a8702f3463515b12ae1040
whopper-sculder <wjhopper510@gmail.com>
2015-10-20 13:27:55 -0400

    add multipicative scaling constant to theme_larger

--- R/ggplotThemes.R
+++ R/ggplotThemes.R
@@ -2,16 +2,18 @@
 #' @import ggplot2
 #' @import grid
 #' @export
-theme_larger <- function()
+theme_larger <- function(magnify = 2)
   theme_grey() +
-  theme(axis.title.x=element_text(size=rel(2),vjust=-.65),
-        axis.title.y=element_text(size=rel(2),vjust=1.5),
-        legend.title = element_text(size=rel(1.6)),
-        legend.text = element_text(size=rel(1.35)),
-        axis.text.x = element_text(size=rel(2)),
-        axis.text.y = element_text(size=rel(2)),
+  theme(axis.title.x=element_text(size=rel(magnify),vjust=-.65),
+        axis.title.y=element_text(size=rel(magnify),vjust=1.5),
+        legend.title = element_text(size=rel(magnify)),
+        legend.text = element_text(size=rel(magnify*.833)),
+        axis.text.x = element_text(size=rel(magnify *.75)),
+        axis.text.y = element_text(size=rel(magnify*.75)),
         axis.ticks.x = element_blank(),
-        plot.title = element_text(size=24))
+        strip.text.x = element_text(size = rel(magnify)),
+        strip.text.y = element_text(size = rel(magnify)),
+        plot.title = element_text(size=rel(magnify*.833)))
 
 #' @import ggplot2
 #' @import grid
@@ -25,6 +27,7 @@ theme_presentation <- function() {
         axis.text.y = element_text(size=rel(3)),
         axis.ticks.x = element_blank(),
         plot.title = element_text(size=36),
+        strip.text.x = element_text(size = rel(magnify*.75)),
         legend.key.height=unit(2,"line"),
         legend.key.width=unit(2,"line"))
 }
