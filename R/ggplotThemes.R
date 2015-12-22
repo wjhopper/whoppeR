e7618074727795872bbdeef5f4dd172d1c98b638
whopper <wjhopper510@gmail.com>
2015-12-22 13:57:27 -0500

    Removed the obsolete 'presentation' ggplot theme
    Added argument to change the base theme in theme_larger
    
    Stop importing grid unnecesarily, ggplot2 handles it

--- R/ggplotThemes.R
+++ R/ggplotThemes.R
@@ -1,9 +1,8 @@
 
 #' @import ggplot2
-#' @import grid
 #' @export
-theme_larger <- function(magnify = 2)
-  theme_grey() +
+theme_larger <- function(magnify = 2, base_theme = theme_gray)
+  base_theme() +
   theme(axis.title.x=element_text(size=rel(magnify),vjust=-.65),
         axis.title.y=element_text(size=rel(magnify),vjust=1.5),
         legend.title = element_text(size=rel(magnify)),
@@ -13,21 +12,6 @@ theme_larger <- function(magnify = 2)
         axis.ticks.x = element_blank(),
         strip.text.x = element_text(size = rel(magnify)),
         strip.text.y = element_text(size = rel(magnify)),
-        plot.title = element_text(size=rel(magnify*.833)))
-
-#' @import ggplot2
-#' @import grid
-theme_presentation <- function() {
-  theme_grey() +
-  theme(axis.title.x=element_text(size=rel(3),vjust=-.5),
-        axis.title.y=element_text(size=rel(3),vjust=1.5),
-        legend.title = element_text(size=rel(3)),
-        legend.text = element_text(size=rel(3)),
-        axis.text.x = element_text(size=rel(3)),
-        axis.text.y = element_text(size=rel(3)),
-        axis.ticks.x = element_blank(),
-        plot.title = element_text(size=36),
-        strip.text.x = element_text(size = rel(magnify*.75)),
-        legend.key.height=unit(2,"line"),
-        legend.key.width=unit(2,"line"))
-}
+        plot.title = element_text(size=rel(magnify*.833)),
+        legend.key.height = unit(2,"line"),
+        legend.key.width = unit(2,"line"))
