a02c6808ce61f6f1023e97540139242c93b5c9cc
whopper <wjhopper510@gmail.com>
2015-12-23 20:24:56 -0500

    Replaced functionality of the three functions betaABfromMeanKappa, betaABfromModeKappa, and betaABfromMeanSD, with a single function, betaParams.
    Based on the combination of arguments to betaParams, it will dispatch to the appropriate solver (ABfromMeanKappa, ABfromModeKappa, orABfromMeanSD) to find the beta shape parameters.
    
    betaParams is exported from the package namespace, while ABfromMeanKappa, ABfromModeKappa, and ABfromMeanSD are not.

--- R/stats.R
+++ R/stats.R
@@ -60,30 +60,62 @@ betaMoments <- function(alpha, beta){
 }
 
 
-#' betaABfromMeanKappa
-#' Beta distribution shape parameters recovered from mean and concentration:
-#' @param mean numeric scalar in range (0,1)
-#' @param kappa numeric scalar in range (0,Inf)
+#' Beta a & b
+#' Determine the shape parameters of a beta distribution, given its central tendency and spread.
+#' See 'Details' section for constraints on which arguments may be used together.
+#'
+#' @param mean A numeric scalar in range (0,1)
+#' @param mode A numeric scalar in range (0,1)
+#' @param kappa A numeric scalar in range (0,Inf).
+#' @param sd A numeric scalar in range (0,Inf)
 #'
 #' @return Named list with 2 elements:
 #' a = Beta shape parameter #1,
 #' b = Beta shape parameter #2
-#' @export
 #'
-#' @author John Krushke, in Kruschke, J. K. (2015). Doing Bayesian Data Analysis, Second Edition:
-#' A Tutorial with R, JAGS, and Stan. Academic Press / Elsevier.
+#' @details Only two arguments may be specified together. Specifically, one parameter
+#' relating to the distributions central tendency (i.e. mean or mode) must be specified with
+#' one parameter relating to its spread (i.e. kappa or sd). Additionally, not all combinations
+#' of (mean,mode) and (kappa, sd) are valid; Solutions exist using mean and kappa, mean and sd,
+#' and mode and kappa. No solution for the shape parameters using the mode and kappa is used.
+#'
+#' The kappa parameter refers to the concentration of the beta distribution. As such, it is
+#' negatively related to the standard deviation.
 #'
 #' @examples
-#' betaABfromMeanKappa(mean = .6, kappa=15)
-betaABfromMeanKappa = function( mean , kappa ) {
-  if ( mean <=0 | mean >= 1) stop("must have 0 < mean < 1")
-  if ( kappa < 0 ) stop("kappa must be > 0")
-  a = mean * kappa
-  b = ( 1.0 - mean ) * kappa
-  return( list( a=a , b=b ) )
+#' betaParams(mean = .6, kappa=15)
+#' betaParams(mode = .25, kappa=15)
+#' betaParams(mean = .6, kappa=15)
+#'
+#' # Throws an error, because you need one measure of central tendency and one of spread, while
+#' # sd and kappa are both measures of spread.
+#' \dontrun{betaParams(sd = .2, kappa=15)}
+#'
+#' @author Will Hopper (2016)
+#' Inspired by functions writen by John Krushke to accompany his "Doing Bayesian Data Analysis" book.
+#' Kruschke, J. K. (2015). Doing Bayesian Data Analysis, Second Edition:
+#' A Tutorial with R, JAGS, and Stan. Academic Press / Elsevier.
+#'
+#' @export
+betaParams = function( mean, mode, kappa, sd ) {
+
+  input <- as.list(match.call())[-1]
+  if (!identical(length(input), 2L)) {
+    stop("Exactly two arguments must be specified")
+  }
+
+  if (setequal(names(input),c("mean","kappa"))) {
+    return(ABfromMeanKappa(mean, kappa))
+  } else if (setequal(names(input),c("mode","kappa"))) {
+    return(ABfromModeKappa(mode,kappa))
+  } else if (setequal(names(input),c("mean","sd"))) {
+    return(ABfromMeanSD(mean, sd))
+  } else {
+    stop("Invalid combination of arguments: Must specify mean & kappa, mean & sd, or mode & kappa")
+  }
 }
 
-#' betaABfromModeKappa
+#' ABfromModeKappa
 #' Beta distribution shape parameters recovered from mode and concentration:
 #' @param mode numeric scalar in range (0,1)
 #' @param kappa numeric scalar in range (0,Inf)
@@ -91,14 +123,13 @@ betaABfromMeanKappa = function( mean , kappa ) {
 #' @return Named list with 2 elements:
 #' a = Beta shape parameter #1,
 #' b = Beta shape parameter #2
-#' @export
 #'
 #' @author John Krushke, in Kruschke, J. K. (2015). Doing Bayesian Data Analysis, Second Edition:
 #' A Tutorial with R, JAGS, and Stan. Academic Press / Elsevier.
 #'
 #' @examples
-#' betaABfromModeKappa(mode = .4, kappa=10)
-betaABfromModeKappa = function( mode , kappa ) {
+#' \dontrun{ABfromModeKappa(mode = .4, kappa=10)}
+ABfromModeKappa = function( mode , kappa ) {
   if ( any(mode <=0) || any(mode >= 1)) stop("must have 0 < mode < 1")
   if ( any(kappa <2) ) stop("kappa must be > 2 for mode parameterization")
   a = mode * ( kappa - 2 ) + 1
@@ -106,28 +137,49 @@ betaABfromModeKappa = function( mode , kappa ) {
   return( list( a=a , b=b ) )
 }
 
-#' betaABfromMeanSD
+#' ABfromMeanSD
 #' Beta distribution shape parameters recovered from mean and standard deviation
 #' @param mean numeric scalar in range (0,1)
-#' @param sdnumeric scalar in range (0,Inf)
+#' @param sd numeric scalar in range (0,Inf)
 #'
 #' @return Named list with 2 elements:
 #' a = Beta shape parameter #1,
 #' b = Beta shape parameter #2
-#' @export
 #'
 #' @author John Krushke, in Kruschke, J. K. (2015). Doing Bayesian Data Analysis, Second Edition:
 #' A Tutorial with R, JAGS, and Stan. Academic Press / Elsevier.
 #'
 #' @examples
-#' betaABfromMeanSD(mean =.5, sd = 1)
-betaABfromMeanSD = function( mean , sd ) {
+#' \dontrun{ABfromMeanSD(mean =.5, sd = 1)}
+ABfromMeanSD = function( mean , sd ) {
   if (any(mean <=0) || any(mean >= 1)) stop("must have 0 < mean < 1")
   if ( any(sd <= 0 )) stop("sd must be > 0")
   mean <- round(mean,10)
   sd <- round(sd,10)
   kappa <- mean*(1-mean)/sd^2 - 1
   if ( any(kappa < 0 )) stop("invalid combination of mean and sd")
+  a = mean * kappa
+  b = ( 1.0 - mean ) * kappa
+  return( list( a=a , b=b ) )
+}
+
+#' ABfromMeanKappa
+#' Beta distribution shape parameters recovered from mean and concentration:
+#' @param mode numeric scalar in range (0,1)
+#' @param kappa numeric scalar in range (0,Inf)
+#'
+#' @return Named list with 2 elements:
+#' a = Beta shape parameter #1,
+#' b = Beta shape parameter #2
+#'
+#' @author John Krushke, in Kruschke, J. K. (2015). Doing Bayesian Data Analysis, Second Edition:
+#' A Tutorial with R, JAGS, and Stan. Academic Press / Elsevier.
+#'
+#' @examples
+#' \dontrun{ABfromMeanKappa(mean = .6, kappa = 10)}
+ABfromMeanKappa = function( mean , kappa ) {
+  if ( mean <=0 | mean >= 1) stop("must have 0 < mean < 1")
+  if ( kappa < 0 ) stop("kappa must be > 0")
   a = mean * kappa
   b = ( 1.0 - mean ) * kappa
   return( list( a=a , b=b ) )
