b5b199f3d3f4e4d58adb7013b6f2250122420c9a
whopper-sculder <wjhopper510@gmail.com>
2016-03-24 16:21:43 -0400

    removed some dead plotting code from performanceBins in summarising.R

--- R/summarising.R
+++ R/summarising.R
@@ -98,26 +98,3 @@ performanceBins <- function(data, bin_by,
   return(binned)
 
 }
-#
-#
-#   tm <- as.data.frame(xtabs( ~ bin,tailoring_means)) %>%
-#     mutate(Freq = Freq/sum(Freq))
-
-#   tailor_plot <- ggplot(tm, aes(x=bin,y=Freq)) +
-#     geom_bar(stat='identity') +
-#     scale_y_continuous("Proportion of Subjects", limits = c(0,max(tm$Freq+.025))) +
-#     geom_text(aes(label =c("6 Secs","5 secs","4 Secs"), y= Freq+.025)) +
-#     xlab("Practice List Performance") +
-#     theme_larger() +
-#     ggtitle('Percent of Subjects with Specific Performance Levels')
-#
-#   if (return.plot & return.data) {
-#     return(list(data = tm, figure = tailor_plot))
-#   } else if (return.plot & !return.data) {
-#     return(tailor_plot)
-#   } else if  (!return.plot & return.data) {
-#     return(tm)
-#   } else {
-#     return(invisible())
-#   }
-# }
