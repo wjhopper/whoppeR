b0ad76c12530d295be899755dd2066b72bfe114a
whopper-sculder <wjhopper510@gmail.com>
2016-04-11 09:48:35 -0400

    changed doParallelCluster to doClusterSetup.
    Returns null if "foreach" and "doParallel" are not found. Sets up logging to a tempfile. Tempfile path is returned in a list, along with the cluster handle.

--- R/parallelHelpers.R
+++ R/parallelHelpers.R
@@ -1,14 +1,18 @@
 #' @export
-tryDoParallelCluster <- function(nCores){
+doParallelCluster <- function(nCores){
+
     packs <- requireNamespace(c('foreach', 'doParallel'), quietly = TRUE)
+
     if (all(packs)) {
       library(foreach)
       library(doParallel)
-      cl <- makeCluster(nCores)
+      out <- tempfile(pattern = "parallel", fileext = ".log")
+      cluster <- makeCluster(nCores, outfile = out)
       registerDoParallel(cl)
-      #     registerDoRNG(456)
-      return(list(success = TRUE, handle = cl))
+      return(list(cluster,logfile = out))
+
     } else {
       warning("Packages foreach and doParallel not found to do parallel processing")
-      return(list(success = FALSE, handle = NULL))    }
+      return(NULL)
+    }
 }
